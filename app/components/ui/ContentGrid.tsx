d860b09ffe5fe31dc8383f9b814330163c598609
genieprinie <geniexpn@gmail.com>
2025-04-26 22:59:33 +0200

    Update img size

--- app/components/ui/ContentGrid.tsx
+++ app/components/ui/ContentGrid.tsx
@@ -29,24 +29,14 @@ export default function ContentGrid({
       </h2>
 
       <div className="col-start-1 col-span-full md:col-span-7 pb-8 md:pb-12 relative h-[500px]">
-        {image.fill ? (
-          <Image
-            src={image.src}
-            alt={image.alt}
-            fill
-            sizes={image.sizes || "(max-width: 768px) 100vw, 50vw"}
-            className="object-cover"
-            priority={image.priority}
-          />
-        ) : (
-          <Image
-            src={image.src}
-            alt={image.alt}
-            width={image.width || 1000}
-            height={image.height || 1000}
-            className="w-full h-auto"
-          />
-        )}
+        <Image
+          src={image.src}
+          alt={image.alt}
+          fill
+          sizes={image.sizes || "(max-width: 768px) 100vw, 50vw"}
+          className="object-cover"
+          priority={image.priority}
+        />
       </div>
 
       <p className="mt-16 mb-60 col-start-1 col-span-full md:col-start-6 md:col-span-6">
