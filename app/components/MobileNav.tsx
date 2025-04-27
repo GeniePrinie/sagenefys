463951bba7db3bda9b85c588a6d037269f0ffc12
genieprinie <geniexpn@gmail.com>
2025-04-27 17:20:39 +0200

    Add drawer for mobile menu

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import CustomLink from "./ui/Link";
+import { Drawer } from "@mui/material";
 
 const menuItems = [
   { text: "Hjem", path: "/" },
@@ -21,7 +22,7 @@ export default function MobileNav() {
       {/* Mobile menu button */}
       <div className="flex md:hidden">
         <button
-          className="md:hidden relative top-0 right-0 z-50 bg-muted-fg text-white rounded-full px-4 py-2"
+          className="md:hidden relative top-0 right-0 z-50 bg-muted-fg text-white rounded-full px-4 py-2 cursor-pointer"
           onClick={() => setOpen(true)}
           aria-label="Open menu"
         >
@@ -29,42 +30,45 @@ export default function MobileNav() {
         </button>
       </div>
 
-      {/* Overlay */}
-      {open && (
-        <div
-          className="fixed inset-0 z-40 bg-opacity-90"
-          onClick={() => setOpen(false)}
-        />
-      )}
-
-      {/* Drawer */}
-      <div
-        className={`fixed top-0 right-0 h-full w-72 bg-white shadow-lg z-50 transform transition-transform duration-300 ${
-          open ? "translate-x-0" : "translate-x-full"
-        }`}
+      <Drawer
+        anchor="right"
+        open={open}
+        onClose={() => setOpen(false)}
+        PaperProps={{
+          sx: {
+            width: 288,
+            backgroundColor: "white",
+          },
+        }}
       >
-        {/* Close button */}
-        <button
-          className="absolute top-6 right-4 bg-muted-fg text-white rounded-full px-4 py-2 mt-6.5"
-          onClick={() => setOpen(false)}
-          aria-label="Close menu"
-        >
-          Lukk
-        </button>
-        {/* Menu List */}
-        <nav className="flex flex-col gap-6 pt-24 pl-8">
-          {menuItems.map((item) => (
-            <CustomLink
-              key={item.text}
-              href={item.path}
-              isActive={pathname === item.path}
+        <div className="flex flex-col h-full">
+          {/* Close button */}
+          <div className="flex justify-end pr-4 pt-[50px] mb-8">
+            <button
+              className="bg-muted-fg text-white rounded-full px-4 py-2 cursor-pointer"
               onClick={() => setOpen(false)}
+              aria-label="Close menu"
             >
-              {item.text}
-            </CustomLink>
-          ))}
-        </nav>
-      </div>
+              Lukk
+            </button>
+          </div>
+
+          {/* Menu List */}
+          <nav className="flex flex-col gap-6 px-8">
+            {menuItems.map((item) => (
+              <CustomLink
+                key={item.text}
+                href={item.path}
+                isActive={pathname === item.path}
+                onClick={() => setOpen(false)}
+                className="w-fit"
+              >
+                {item.text}
+              </CustomLink>
+            ))}
+          </nav>
+        </div>
+      </Drawer>
     </>
   );
 }
