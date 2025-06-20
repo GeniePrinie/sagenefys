262cdf93685e7584c348da3023398ee19e2a3588
Genie Prinyanut <100139381+GeniePrinie@users.noreply.github.com>
2025-06-20 10:47:52 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Only needed if using next/image
+  },
 };
 
 export default nextConfig;
